8bdafaa22ae08ad94cbe54034bef42cfac5cedaf
psnewman <novitskiyp@gmail.com>
2024-12-15 00:44:05 +0100

    Memo field added
    • TextAreaView added
    • Memo field removed
    • CardView and CardViewModel updated for supporting memo input
    • MyTimersSectionView: background color issue fixed
    • CardView elements padding updated

---
+++ BQE/Main Navigation/Home/Sections/CatchUp/Components/TextAreaView.swift
@@ -0,0 +1,18 @@
+//
+//  TextAreaView.swift
+//  BQE
+//
+//  Created by Paul Newman on 12/13/24.
+//
+
+import SwiftUI
+
+struct TextAreaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextAreaView()
+}
